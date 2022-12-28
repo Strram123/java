f89e7c743013e9e8ac773c802ee1d57b8b33c3a5
Strram123 <121536281+Strram123@users.noreply.github.com>
2022-12-28 14:29:11 +0530

    Create TypecastDemo.java

---
+++ TypecastDemo.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class TypecastDemo {
+
+	public static void main(String[] args) {
+		double d=10.8;
+		
+		int i=(int)d;
+
+		System.out.println(i);	
+		//String.format("%.2f ", i);
+		
+		
+	}
+
+}
