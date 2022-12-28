91169d2ffc2bfaf318dff96be35ab3067332d429
Strram123 <121536281+Strram123@users.noreply.github.com>
2022-12-28 13:19:19 +0530

    primitive datatypes values

---
+++ ValuesOfDataTypes.java
@@ -0,0 +1,23 @@
+package mypackage;
+
+public class ValuesOfDataTypes {
+
+	public static void main(String[] args) {
+		System.out.println("byte:"+Byte.MIN_VALUE);
+		System.out.println("byte:"+Byte.MAX_VALUE);
+		System.out.println("short:"+Short.MIN_VALUE);
+		System.out.println("short:"+Short.MAX_VALUE);
+		System.out.println("int:"+Integer.MIN_VALUE);
+		System.out.println("int:"+Integer.MAX_VALUE);
+		System.out.println("long:"+Long.MIN_VALUE);
+		System.out.println("long:"+Long.MAX_VALUE);
+		System.out.println("float:"+Float.MIN_VALUE);
+		System.out.println("float:"+Float.MAX_VALUE);
+		System.out.println("double:"+Double.MIN_VALUE);
+		System.out.println("double:"+Double.MAX_VALUE);
+		System.out.println("char:"+Character.MIN_VALUE);// \u0000
+		System.out.println("char:"+Character.MAX_VALUE);// \uffff
+		
+	}
+
+}
