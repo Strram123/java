a53cca133cc74b05e36f891da42d8d483557e44e
Ramkumar <121536281+Strram123@users.noreply.github.com>
2023-03-10 16:14:24 +0530

    Update OperatorsDemo.java

--- OperatorsDemo.java
+++ OperatorsDemo.java
@@ -3,18 +3,18 @@ package mypackage;
 public class OperatorsDemo {
 
 	public static void main(String[] args) {
-		int a=5;
-		int b=4;
-		int c;
-		c=a&b;
-		System.out.println(" and Result="+c);
-		System.out.println("and & binary value "+Integer.toBinaryString(a&b));
-		c=a|b;
-		System.out.println("or result="+c);
-		System.out.println("or | binaryvalue "+Integer.toBinaryString(a|b));
-		c=a^b;
-		System.out.println("xor result="+c);
-		System.out.println("xor ^ binaryvalue "+Integer.toBinaryString(a^b));
+		int number1=5;
+		int  number2=4;
+		int number3;
+		 number3= number1& number2;
+		System.out.println(" and Result="+ number3);
+		System.out.println("and & binary value "+Integer.toBinaryString( number1& number2));
+		 number3= number1| number2;
+		System.out.println("or result="+ number3);
+		System.out.println("or | binaryvalue "+Integer.toBinaryString( number1| number2));
+		 number3= number1^ number2;
+		System.out.println("xor result="+ number3);
+		System.out.println("xor ^ binaryvalue "+Integer.toBinaryString( number1^ number2));
 		
 	}
 
