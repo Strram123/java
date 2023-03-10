ab9ff868c7dfe05d2c195d22fa1669233185835b
Ramkumar <121536281+Strram123@users.noreply.github.com>
2023-03-10 16:10:52 +0530

    Update StringDemo.java

--- StringDemo.java
+++ StringDemo.java
@@ -8,13 +8,13 @@ public class Stringdemo {
 		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter a string:");
-		String s=sc.nextLine();
+		String word=sc.nextLine();
 		String str=" ";
-		String[] arr=s.split(" ");
+		String[] arr=word.split(" ");
 		for(int i=0;i<arr.length;i++)
 		{
-			char c=arr[i].charAt(0);
-			String c1=String.valueOf(c).toUpperCase();
+			char charecter=arr[i].charAt(0);
+			String word2=String.valueOf(chreacter).toUpperCase();
 			String sub=arr[i].substring(1);
 			str=str+c1+sub+" ";
 		}
